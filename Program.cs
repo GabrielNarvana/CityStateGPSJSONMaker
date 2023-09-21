9339b0ac9aa28391c5f0783ca3b12faf245f9c73
GabrielNarvana <gabriel.j.l@hotmail.com>
2023-09-21 14:50:02 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bem vindo ao CityStateGPSJSONMaker® by Narvana");
+        Console.WriteLine("Sim, o Pai da cria é o Narvana, recuse imitações");
+
+        // Ler o JSON de estados e municípios
+        string estadosMunicipiosJson = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "locais.json"));
+        string estadosJson = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "estados.json"));
+        string municipiosJson = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "municipios.json"));
+        // Converter os JSONs para objetos
+        var estadosMunicipios = JsonConvert.DeserializeObject<JObject>(estadosMunicipiosJson);
+        var estados = JsonConvert.DeserializeObject<List<Estado>>(estadosJson);
+        var municipios = JsonConvert.DeserializeObject<List<Municipio>>(municipiosJson);
+
+        // Mapear as latitudes dos estados
+        var estadosComLatitudes = new Dictionary<string, double>();
+        foreach (var estado in estados)
+        {
+            estadosComLatitudes[estado.UF] = estado.Latitude;
+        }
+
+        // Mapear as latitudes dos municipios
+        var municipiosComLatitudes = new Dictionary<string, double>();
+        foreach (var municipio in municipios)
+        {
+            municipiosComLatitudes[municipio.Nome.ToUpper()] = municipio.Latitude;
+            
+        }
+
+        // Construir o novo JSON no formato desejado
+        var novoJson = new JObject();
+        var estadosArray = new JArray();
+
+        foreach (var estado in estadosMunicipios["estados"])
+        {
+            string sigla = estado["sigla"].ToString();
+            string nome = estado["nome"].ToString();
+
+            var estadoComLatitude = new JObject();
+            estadoComLatitude["sigla"] = sigla;
+            estadoComLatitude["nome"] = nome;
+            var cidadesArray = new JArray();
+
+            foreach (var cidade in estado["cidades"])
+            {
+
+                //var municipio = municipios.Find(m => m.Nome == nomeCidade);
+                //double latitude = municipio != null ? municipio.Latitude : 0.0;
+
+                string nomeCidade = cidade.ToString().ToUpper();
+                double latitude = (municipiosComLatitudes.ContainsKey(nomeCidade) ? municipiosComLatitudes[nomeCidade] : estadosComLatitudes[sigla]);
+
+                var cidadeComLatitude = new JObject();
+                cidadeComLatitude["Municipio"] = nomeCidade;
+                cidadeComLatitude["Latitude"] = latitude;
+
+                cidadesArray.Add(cidadeComLatitude);
+            }
+
+            estadoComLatitude["cidades"] = cidadesArray;
+            estadosArray.Add(estadoComLatitude);
+        }
+
+        novoJson["estados"] = estadosArray;
+
+        // Converter o novo JSON para string e imprimir
+        string novoJsonString = novoJson.ToString();
+        Console.WriteLine(novoJsonString);
+
+        // Salvar o novo JSON em um arquivo
+        File.WriteAllText("novo_estados_municipios.json", novoJsonString);
+    }
+}
+
+
+class Estado
+{
+    public int CodigoUF { get; set; }
+    public string UF { get; set; }
+    public string Nome { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Regiao { get; set; }
+}
+
+class Municipio
+{
+    public int CodigoIBGE { get; set; }
+    public string Nome { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int Capital { get; set; }
+    public int CodigoUF { get; set; }
+    public int SiafiID { get; set; }
+    public int DDD { get; set; }
+    public string FusoHorario { get; set; }
+}
